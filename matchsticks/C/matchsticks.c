e6acce875c57095980ac62af50669faf0337d4e3
strahlistvan <strahlistvan0@gmail.com>
2019-11-01 23:01:09 +0100

    refactor: make code even more messy

--- matchsticks/C/matchsticks.c
+++ matchsticks/C/matchsticks.c
@@ -1,81 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char ** argv) {
 
-    char s,c,t,i=0;
+    //p - contains remaining sticks count, computer/player turn and user input (sticks taken)
+    char * p = (char *) malloc(4*sizeof(char));
+    char * s = (char *) malloc(256*sizeof(char)); //user input characters
 
-    c = 1;
-    s = 21;
+    *p = 0x15;
+    *(p+1) = 1;
 
-    printf("there are 21 matchsticks on the table\n");
-    printf("computer's first\n");
+    printf("there are %d matchsticks on the table\ncomputer's first\n", *p);
 
-    while (s != 0) {
-        if (c) {
+    while (*p != 0) {
+        if (p[1]) {
             printf("computer's thinking\n");
-            t = !((s - 1) % 4) ? 1 : (s - 1) % 4;
+            p[2] = !((*p - 1) % 4) ? 1 : (*p - 1) % 4;
 
-            printf("computer takes %d\n", t);
+            printf("computer takes %d\n", p[2]);
             // take or end
-            if (c) { // if computer
-                if (s - t <= 0) { //computer takes last
+            if (p[1]) { // if computer
+                if (p[0] - p[2] <= 0) { //computer takes last
                     printf("game over\nthe winner is you\n");
-                    s = 0;
+                    p[0] = 0;
                 } else { //computer takes
-                    s = s - t;
+                    p[0] = p[0] - p[2];
                 }
             }
             else {
-                if (s - t <= 0) { //you take last
-                    s = 0;
+                if (p[0] - p[2] <= 0) { //you take last
+                    p[0] = 0;
                     printf("game over\n");
                     printf("the winner is computer\n");
                 } else { // you take
-                    s -= t;
+                    p[0] -= p[2];
                 }
             }
 
             //your turn
-            c = !c;
+            p[1] = !p[1];
 
         } else {
             printf("it is your turn\n");
-            int t;
 
             do {
-                printf("you can take 1, 2 or 3!\n");
-                printf("so how many sticks will you take?\n");
-                scanf("%d", &t);
-            } while (!(t == 1 || t == 2 || t == 3));
+                printf("you can take 1, 2 or 3!\nso how many sticks will you take?\n");
+                //handle invalid input
+                scanf("%s", s);
+                if (s[1] != '\0'){ p[2] = 0; }
+                else { p[2] = s[0] - '0'; }
+            } while (!(p[2] == 1 || p[2] == 2 || p[2] == 3));
 
             // take or end
-            if (c) { // if computer
-                if (s - t <= 0) { //computer takes last
+            if (p[1]) { // if computer
+                if (p[0] - p[2] <= 0) { //computer takes last
                     printf("game over\nthe winner is you\n");
-                    s = 0;
+                    p[0] = 0;
                 } else { //computer takes
-                    s = s - t;
+                    p[0] = p[0] - p[2];
                 }
             }
             else {
-                if (s - t <= 0) { //you take last
-                    s = 0;
+                if (p[0] - p[2] <= 0) { //you take last
+                    p[0] = 0;
                     printf("game over\n");
                     printf("the winner is computer\n");
                 } else { // you take
-                    s -= t;
+                    p[0] -= p[2];
                 }
             }
 
             //computer turn
-            c = !c;
+            p[1] = !p[1];
         }
 
         //print sticks
-        i = i & 0x0000;
-        printf("there are %d matchstick(s) on the table\n", s);
-        while (i++<s) { printf("|"); }
+        p[3] = *(p+3) & 0x00;
+        printf("there are %d matchstick(s) on the table\n", *p);
+        while ((*(p+3))++ < *p) { printf("|"); }
         printf("\n");
     }
+    free(p);
+    free(s);
     return 0;
 }
